7945b4d68d2b3b9c01b0b9548efdcd4924546640
Shimaa Reda <81780715+Shimaa-reda@users.noreply.github.com>
2024-10-23 16:03:10 +0300

    install primevue,bootstrap

--- src/main.js
+++ src/main.js
@@ -3,9 +3,19 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Dialog from 'primevue/dialog';
 
-const app = createApp(App)
 
-app.use(router)
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.use(router).component('Dialog', Dialog).mount("#app");
