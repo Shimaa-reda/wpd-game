392b0b3a759396cd0aa22c986c64b8ffdcfb7009
asemsalem <asemsalem@gmail.com>
2024-10-27 16:26:39 +0300

    remove test data

--- src/views/Day.vue
+++ src/views/Day.vue
@@ -90,7 +90,6 @@ const showFact = ref(false);
 const currentFact = ref({});
 let idleTimeout = null; // Holds the idle timeout reference
 
-// console.log("idel",idleTimeout)
 const originalFacts = [
   {
     id: 1,
@@ -175,37 +174,31 @@ const usedFacts = ref([]);
 onMounted(() => {
   
   loadCounts();
-  console.log("idel onmounted",idleTimeout)
   resetIdleTimer();
   
   const body = document.body;
-  const date = new Date("1 November 2024");
+  const date = new Date("");
   const day = date.getDate();
   const month = date.getMonth() + 1; // getMonth() returns 0-11
-  console.log("day",day,"month",month);
 
   let backgroundImage = '';
 
   // Define your background images based on day and month
   if (month === 10 && day === 29) {
     backgroundImage = new URL('@/assets/images/day1.png', import.meta.url).href;
-    console.log('First Day');
   } else if (month === 10 && day === 30) {
     backgroundImage = new URL('@/assets/images/day2.png', import.meta.url).href;
-    console.log('Second Day');
   } else if (month === 10 && day === 31) {
     backgroundImage = new URL('@/assets/images/day3.png', import.meta.url).href;
-    console.log('Third Day');
   } else if (month === 11 && day === 1) {
-    console.log('Fourth Day');
-    router.push({ name: 'lastday' });
+  } else {
+    backgroundImage = new URL('@/assets/images/day1.png', import.meta.url).href;
   }
 
   body.style.backgroundImage = `url(${backgroundImage})`;
 });
 
 onUnmounted(() => {
-  console.log("idel onunmounted",idleTimeout)
   clearTimeout(idleTimeout);
 });
 function showRandomFact() {
